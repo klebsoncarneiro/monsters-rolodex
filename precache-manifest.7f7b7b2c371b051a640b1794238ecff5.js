9c30d885cd9ebffd8efa4f7d060451c9aa70f341
Klebson Carneiro <klebsoncarneiro@gmail.com>
2019-12-29 15:22:58 -0300

    Updates

---
+++ precache-manifest.7f7b7b2c371b051a640b1794238ecff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a551ee5eb9a1059f0bdf6a2f00853b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8101178964f1944f0b0b",
+    "url": "/monsters-rolodex/static/css/main.7235b3ef.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8101178964f1944f0b0b",
+    "url": "/monsters-rolodex/static/js/main.6d046f43.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
